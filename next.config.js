241ffaf706346e92f0304aaf8dcb55635e1c184c
rajithaExpernetic <rajitha@expernetic.com>
2024-01-27 22:32:02 +0530

    feat:done

--- next.config.js
+++ next.config.js
@@ -3,6 +3,7 @@ const { withNextVideo } = require('next-video/process');
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+    reactStrictMode: false,
     images: {
         domains: ['tailwindui.com'],
     },
